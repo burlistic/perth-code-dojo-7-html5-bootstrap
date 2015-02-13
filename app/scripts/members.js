195dea5875b92a21ff9a1a04417aad84557ab92b
burlistic <burlistic@yahoo.co.uk>
2015-02-13 11:46:51 +0800

    Added simple JS to load in members from Express API

--- app/scripts/members.js
+++ app/scripts/members.js
@@ -1,6 +1,16 @@
 
 
-$.get( "http://localhost:3000/api/users", function( data ) {
+$.getJSON( "http://localhost:3000/api/users", function( data ) {
   $( ".result" ).html( data );
-  alert( "Load was performed." );
+  //alert(data);
+  //alert( "Load was performed." );
+
+  var items = [];
+  $.each( data.users, function( key, val ) {
+    items.push( "<li id='" + key + "'>" + val.firstName + " " + val.lastName + "</li>" );
+  });
+
+$( ".members" ).html(items);
+
+
 });
