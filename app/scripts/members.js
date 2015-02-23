51737204ddcdd1a23a7e24bd48d46cb137aed341
Kai Dombrowski <mail@kaidombrowski.com>
2015-02-23 20:11:03 +0800

    Displaying group members

--- app/scripts/members.js
+++ app/scripts/members.js
@@ -2,17 +2,31 @@
 
 (function(){
 
-$.getJSON( "http://perthcodedojo7service-pmezdnzk4k.elasticbeanstalk.com/api/users/", function( data ) {
-  $( ".result" ).html( data );
-
-  var items = [];
-  $.each( data.users, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val.firstName + " " + val.lastName + "</li>" );
-  });
-
-$( ".members" ).html(items);
-
-
+$.getJSON( "https://api.meetup.com/2/members?group_id=15388882&key={KEY}&callback=?", function( data ) {	
+	var users = [];
+    $.each( data.results, function( key, val ) {
+		var bio = "";
+		var twitterString = "";
+		var photoLink = "";
+		
+		if (val.bio) {
+			bio = "Bio: " + val.bio + "<br>";
+		}
+		
+		if (val.photo) {
+			photoLink = val.photo.thumb_link;
+			photoLink = "<img src=" + photoLink + "><br>"		
+		}
+		
+		if (val.other_services.twitter) {
+			var twitterHandle = val.other_services.twitter.identifier;
+			twitterString = "Twitter: " + twitterHandle + "<br>";
+		}
+		
+		users.push( "<div id='" + key + "'>" + "Name: " + val.name + "<br>" + bio + photoLink + twitterString + "</div><hr>" );
+    });
+	
+	$( ".members" ).html(users);
 });
 
 }());
